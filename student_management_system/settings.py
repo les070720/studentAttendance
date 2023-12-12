8e13a70371f6de032a7fb9db988484820712b528
les070720 <mlvmesclanda@tip.edu.ph>
2023-12-13 01:59:02 +0800

    changes to settings.py

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] # new
 STATIC_ROOT = BASE_DIR / "staticfiles" # new
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
